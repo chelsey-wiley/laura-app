00744527863fb57b9a1e0e591ce9ccbad0843a4a
Chelsey Wiley <Wiley.Chelsey@gmail.com>
2017-02-03 14:47:18 -0600

    updating port for heroku

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
+port = process.env.PORT || 5112;
 
-app.listen(5112, function() {
-  console.log('listening on port 5112.');
+app.listen(port, function() {
+  console.log('listening on PORT or locally: 5112.');
 });
